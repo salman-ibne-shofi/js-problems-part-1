363c70ac938a7abdb3eb36f80ac347e79c244173
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-02-05 18:34:26 +0600

    math abs round ceil floor and random number

---
+++ math.js
@@ -0,0 +1,29 @@
+// const Math = {
+//     min: function min(num1, num2){
+//         return num2;
+//     }
+// }
+
+// console.log(Math.max);
+
+// const min = math.min
+
+const min = Math.min(45, 21, 65, 99, 126, 5, -99);
+console.log(min);
+const max = Math.max(45, 21, 65, 99, 126, 5, -99);
+console.log(max);
+
+console.log(Math.PI)
+console.log(Math.abs(88))
+console.log(Math.round(2.25))
+console.log(Math.round(2.85))
+console.log(Math.round(4.5))
+console.log(Math.floor(4.5))
+console.log(Math.floor(4.99))
+console.log(Math.ceil(4.99))
+console.log(Math.ceil(4.00000001))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+const rand = Math.round(Math.random()*10)
+console.log(rand)
